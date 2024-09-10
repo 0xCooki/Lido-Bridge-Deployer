d023e6b5c3824d1d69031c8bbb5309348925c41e
William <william.ridge.95@gmail.com>
2024-09-10 10:26:49 +1000

    caterpie

--- scripts/DeployL1.s.sol
+++ scripts/DeployL1.s.sol
@@ -5,6 +5,8 @@ import "forge-std/Script.sol";
 
 import {TokenRateNotifier} from "@lido/lido/TokenRateNotifier.sol";
 import {OpStackTokenRatePusher} from "@lido/optimism/OpStackTokenRatePusher.sol";
+import {L1LidoTokensBridge} from "@lido/optimism/L1LidoTokensBridge.sol";
+import {OssifiableProxy} from "@lido/proxy/OssifiableProxy.sol";
 
 /*
 - Rate Notifier - this seems to hook directly into Lido core
@@ -12,32 +14,59 @@ import {OpStackTokenRatePusher} from "@lido/optimism/OpStackTokenRatePusher.sol"
 - Bridge proxy / impl
 */
 contract DeployL1 is Script {
+    /// Rate notifier
     address public owner = 0x42e84F0bCe28696cF1D254F93DfDeaeEB6F0D67d;
-    address public lidoCore = 0x3e3FE7dBc6B4C189E7128855dD526361c49b40Af;
+    address public lidoCore = 0x3e3FE7dBc6B4C189E7128855dD526361c49b40Af; /// @dev steth
 
+    /// Rate pusher
     address public optimismMessenger = 0x13931872294360cAc551BA3801f061c4C86F0725;
     address public wstETH = 0xB82381A3fBD3FaFA77B3a7bE693342618240067b;
     address public accountingOracle = 0xd497Be005638efCf09F6BFC8DAFBBB0BB72cD991;
-    address public tokenRateOracle = 0xB34F2747BCd9BCC4107A0ccEb43D5dcdd7Fabf89; /// @dev corresponds to the address on the L2
+    address public tokenRateOracle = address(0); /// @dev corresponds to the address on the L2 - so this must come later (or via create2)
     uint32 public gaslimitForPushing = 300000;
+    
+    /// Bridge
+    address public l2TokenBridge = address(0); /// @dev also corresponds to the address on the L2 
+    address public l1TokenNonRebasable = 0xB82381A3fBD3FaFA77B3a7bE693342618240067b; /// @dev wsteth
+    /// address public l1TokenRebasable = lidoCore;
+    address public l2TokenNonRebasable = address(0); /// @dev wstETH on the L2
+    address public l2TokenRebasable = address(0);    /// @dev stETH om the L2
+    /// address public accountingOracle = accountingOracle;
 
     TokenRateNotifier public notifier;
     OpStackTokenRatePusher public pusher;
+    L1LidoTokensBridge public l1BridgeImpl;
+    OssifiableProxy public l1BridgeProxy;
 
     function run() external broadcast {
+        /// Get predicted addresses
+
         /// Rate notifier
         notifier = new TokenRateNotifier(owner, lidoCore);
+
         /// Rate pusher
         pusher = new OpStackTokenRatePusher(optimismMessenger, wstETH, accountingOracle, tokenRateOracle, gaslimitForPushing);
 
-        ///
+        /// Bridge impl
+        l1BridgeImpl = new L1LidoTokensBridge(optimismMessenger, l2TokenBridge, l1TokenNonRebasable, lidoCore, l2TokenNonRebasable, l2TokenRebasable, accountingOracle);
 
+        /// Bridge proxy
+        l1BridgeProxy = new OssifiableProxy(address(l1Bridge), owner, "");
     }
+
+    /// HELPERS ///
 
     modifier broadcast() {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         _;
         vm.stopBroadcast();
+    }
+
+    function _getPredictedAddressess() internal returns (address) {
+        /// token rate oracle
+        /// l2TokenBridge
+        /// l2TokenNonRebasable
+        /// l2TokenRebasable
     }
 }
--- scripts/DeployL2.s.sol
+++ scripts/DeployL2.s.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.25;
 
 import "forge-std/Script.sol";
 
+import {OssifiableProxy} from "@lido/proxy/OssifiableProxy.sol";
+import {ERC20BridgedPermit} from "@lido/token/ERC20BridgedPermit.sol";
+
 /*
 - wstETHERC20BridgePermit proxy / impl
 - stETHERC20RebasableBridgePermit proxy / impl
@@ -11,8 +14,33 @@ import "forge-std/Script.sol";
 - Op Gov Bridge executor
 */
 contract DeployL2 is Script {
-    function run() external broadcast {
+    address public optimismMessenger = 0x13931872294360cAc551BA3801f061c4C86F0725;
 
+    ERC20BridgedPermit public wstETHImpl;
+    OssifiableProxy public wstETHProxy;
+
+    function run() external broadcast {
+        /*
+        /// @param messenger_ L2 messenger address being used for cross-chain communications
+        /// @param l2ERC20TokenBridge_ the bridge address that has a right to updates oracle.
+        /// @param l1TokenRatePusher_ An address of account on L1 that can update token rate.
+        /// @param tokenRateOutdatedDelay_ time period when token rate can be considered outdated.
+        /// @param maxAllowedL2ToL1ClockLag_ A time difference between received l1Timestamp and L2 block.timestamp
+        ///         when token rate can be considered outdated.
+        /// @param maxAllowedTokenRateDeviationPerDayBp_ Allowed token rate deviation per day in basic points.
+        ///        Can't be bigger than BASIS_POINT_SCALE.
+        /// @param oldestRateAllowedInPauseTimeSpan_ Maximum allowed time difference between the current time
+        ///        and the last received token rate update that can be set during a pause.
+        /// @param minTimeBetweenTokenRateUpdates_ Minimum delta time between two
+        ///        L1 timestamps of token rate updates.
+        */
+
+        /// wstETH impl
+        wstETHImpl = new ERC20BridgedPermit();
+
+        /// wstETH proxy
+
+        
     }
 
     modifier broadcast() {
